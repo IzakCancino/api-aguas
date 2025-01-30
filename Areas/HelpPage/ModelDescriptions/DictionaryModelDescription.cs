270301c5678721b2174fe5a74a40438d98730c84
IzakCancino <84246483+IzakCancino@users.noreply.github.com>
2025-01-29 20:07:17 -0600

    Init commit

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api_aguas.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="api_aguas.WebApiApplication" Language="C#" %>
